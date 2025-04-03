d44462c621f506b2e6714bc80a1955cf8f33ad71
ivanmironovs <ivankamir03@gmail.com>
2025-04-03 18:09:54 +0300

    database update

--- app/src/main/java/com/example/habittracker/data/database/HabitDatabase.kt
+++ app/src/main/java/com/example/habittracker/data/database/HabitDatabase.kt
@@ -1,6 +1,8 @@
 package com.example.habittracker.data.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(
@@ -12,6 +14,22 @@ abstract class HabitDatabase : RoomDatabase() {
     abstract fun getHabitDao(): HabitDao
 
     companion object {
-        const val DATABASE_NAME = "habit_tracker_db"
+        private var INSTANCE: HabitDatabase? = null
+        fun getInstance(context: Context): HabitDatabase {
+
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        HabitDatabase::class.java,
+                        "habit_database"
+
+                    ).fallbackToDestructiveMigration().build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
     }
 }
